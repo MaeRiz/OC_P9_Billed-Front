b904c1bd8c20a975b9a9714d284dc5dd0435d78e
Mahery Gonin <mahery.gonin@gmail.com>
2023-08-13 22:19:12 +0200

    FIX sorted date & tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -51,12 +51,12 @@ describe("Given I am connected as an employee", () => {
     test("Then bills should be ordered from earliest to latest", () => {
 
       document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
+      const dates = screen.getAllByText(/[0-9]+\s[A-Za-z]+\.\s[0-9]+/i).map(a => a.innerHTML)
+      const datesSorted = ["4 Avr. 04", "3 Mar. 03", "1 Jan. 01"]
       expect(dates).toEqual(datesSorted)
 
     })
+
     describe("When I click on new bill button", () => {
       test("Then redirect to new bill form", () => {
 
--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -3,6 +3,7 @@ import ErrorPage from "./ErrorPage.js"
 import LoadingPage from "./LoadingPage.js"
 
 import Actions from './Actions.js'
+import { formatDate } from '../app/format.js'
 
 const row = (bill) => {
   return (`
@@ -20,7 +21,10 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.sort((a, b) => new Date(b.date) - new Date(a.date)).map(bill => row(bill)).join("") : ""
+  return (data && data.length) ? data.sort((a, b) => (new Date(b.date) > new Date(a.date)) ? 1 : -1).map(bill => row({
+    ...bill,
+    date: formatDate(bill.date)
+  })).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
